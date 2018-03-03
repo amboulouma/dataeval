8eb4651dd022c9c51d35227591d25cfc16176d84
Amine M. Boulouma <amine.boulouma@flambok.com>
2018-03-04 00:45:55 +0100

    Adding test_cases mail & url

--- algorithmes/algorithmes_test.py
+++ algorithmes/algorithmes_test.py
@@ -14,22 +14,3 @@ def test_url(url):
     match = re.match(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',url)
     return (match)
 
-# def test_date(date_debut, date_fin):
-#     if :
-#         return True
-#     else:
-#         return False
-
-# print(test_url('http://ww.lol.l'))
-
-# def test_code_postal(code_postal):
-#     if :
-#         return True
-#     else:
-#         return False
-
-# def test_code_postal(code_postal):
-#     if :
-#         return True
-#     else:
-#         return False
